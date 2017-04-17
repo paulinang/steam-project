9919c989070ea2253b9d648188d16737fbdb4b7e
qwnpng <qwnpng@gmail.com>
2017-04-17 23:36:22 +0000

    More requests to steam api for user info and owned games

--- server.py
+++ server.py
@@ -19,9 +19,17 @@ def get_user():
     """ Get steam user info """
 
     nickname = request.form.get('nickname')
-    user = requests.get('http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key={}&vanityurl={}'.format(steam_key, nickname))
 
-    return user.json()['response']['steamid']
+    r_id = requests.get('http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key={}&vanityurl={}'.format(steam_key, nickname))
+    steamid = r_id.json()['response']['steamid']
+
+    r_user = requests.get('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={}&steamids={}'.format(steam_key, steamid))
+    profile_url = r_user.json()['response']['players'][0]['profileurl']
+
+    r_games = requests.get('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={}&steamid={}'.format(steam_key, steamid))
+    game_count = r_games.json()['response']['game_count']
+
+    return 'Hi {}, you own {} games and your steam profile url is {}'.format(nickname, game_count, profile_url)
 
 
 if __name__ == "__main__":
