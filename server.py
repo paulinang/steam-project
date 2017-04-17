ecc1bb086e3d4648ea201b9133a0c62c31276e83
qwnpng <qwnpng@gmail.com>
2017-04-17 23:16:34 +0000

    Can get steamid from username input through new app route

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_debugtoolbar import DebugToolbarExtension
+import os
+import requests
 
 app = Flask(__name__)
+steam_key = os.environ['STEAM_API_KEY']
 
 
 @app.route('/')
@@ -10,12 +14,18 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/get_user', methods=['POST'])
+def get_user():
+    """ Get steam user info """
+
+    nickname = request.form.get('nickname')
+    user = requests.get('http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key={}&vanityurl={}'.format(steam_key, nickname))
+
+    return user.json()['response']['steamid']
+
+
 if __name__ == "__main__":
-    #app.debug = False
-
-    #connect_to_db(app, 'asgard_db')
-
-    # Use the DebugToolbar
-    #DebugToolbarExtension(app)
+    app.debug = False
+    DebugToolbarExtension(app)
 
     app.run(port=5000, host='0.0.0.0')
