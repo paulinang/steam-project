f206917694d32d31c03b66a6f627b922fb6f14c0
qwnpng <qwnpng@gmail.com>
2017-04-24 20:27:46 +0000

    Steam API request gets more info about user apps and only gives playhours for games with >0 hours

--- server.py
+++ server.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 import os
 import requests
-
 app = Flask(__name__)
 steam_key = os.environ['STEAM_API_KEY']
 
@@ -26,14 +25,17 @@ def get_user():
     r_user = requests.get('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={}&steamids={}'.format(steam_key, steamid))
     profile_url = r_user.json()['response']['players'][0]['profileurl']
 
-    r_games = requests.get('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={}&steamid={}'.format(steam_key, steamid))
+    r_games = requests.get('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={}&steamid={}&include_appinfo=1'.format(steam_key, steamid))
     game_count = r_games.json()['response']['game_count']
     games = r_games.json()['response']['games']
+
+    played_games = [(game['name'], game['playtime_forever']) for game in games if game['playtime_forever'] > 0]
 
     return jsonify({'steamid': steamid,
                     'profileurl': profile_url,
                     'gamecount': game_count,
-                    'games': games})
+                    'games': games,
+                    'playedgames': played_games})
 
 if __name__ == "__main__":
     app.debug = False
